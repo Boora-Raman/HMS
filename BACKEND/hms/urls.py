2b7e0a653eefd21df00fcbf8c0c2bd1e486b33ab
Boora-Raman <143726797+Boora-Raman@users.noreply.github.com>
2023-11-19 20:31:17 +0530

    Update urls.py

--- BACKEND/hms/urls.py
+++ BACKEND/hms/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from hms import views
 from .views import user_logout
 urlpatterns = [
-    # path('', views.Welcome, name='Welcome'),
-    # path('signin', views.signin, name='signin'),
-    # path('signup', views.signup, name='signup'),
-    # path('rooms', views.rooms, name='rooms'),
-    path('booking/', views.booking, name='booking'),
-    path('blogs/', views.blogs, name='blogs'),
+    path('', views.Welcome, name='Welcome'),
+    path('signin', views.signin, name='signin'),
+    path('signup', views.signup, name='signup'),
+    path('rooms', views.rooms, name='rooms'),
+    path('booking', views.booking, name='booking'),
+    path('blogs', views.blogs, name='blogs'),
     path('offers', views.offers, name='offers'),
     path('gallery', views.gallery, name='gallery'),
     path('booking_verification', views.booking_verification, name='booking_verification'),
